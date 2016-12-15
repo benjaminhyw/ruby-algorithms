c4058991a8fb98ec755f7e05507031e39b1b9109
Ben Flores <benjaminhyw@gmail.com>
2016-12-14 17:59:07 -0800

    Initial commit, add comments to lay out structure of challenge

---
+++ array_shuffle.rb
@@ -0,0 +1,4 @@
+# Recreate the functionality of Array#shuffle
+# Methods that can't be used for this challenge: Array#sample , Array#shuffle , and Array#sort_by
+# The method you create should always return a shuffled version of the provided array
+#  The method you create should not change the original array (non-destructive)
