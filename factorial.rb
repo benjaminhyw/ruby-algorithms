1b125d4d46afb6c4feb6a74afae53d550edbe802
Ben Flores <benjaminhyw@gmail.com>
2016-12-15 17:37:33 -0800

    Solve factorial iteratively

--- factorial.rb
+++ factorial.rb
@@ -1,5 +1,12 @@
 #Calculate the factorial iteratively.
-
+def factorial_iterative(n)
+  num = 1
+  while (n > 1)
+    num = num * n
+    n = n - 1
+  end
+  return num
+end
 
 
 #Calculate the factorial recursively.
